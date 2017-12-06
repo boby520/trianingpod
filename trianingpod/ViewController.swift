5cfa44ac00a5d9601c55924e1e69a6147eeecea0
boby <linhongzhou@linhongzhoudeMacBook-Pro.local>
2017-12-06 12:14:33 +0800

    新建项目

---
+++ trianingpod/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  trianingpod
+//
+//  Created by 林洪州 on 2017/12/6.
+//  Copyright © 2017年 林洪州. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
